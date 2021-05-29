c5847c0ea648c9b2131c62ad99d615ec696207d0
Nitin Sijwali <NITIN.SIJWALI@questdot.io>
2021-05-29 12:19:23 +0530

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import styles from './stylesheet';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { Text, View, Button, SafeAreaView } from 'react-native';
+import Home from './src/screens/Home';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+	return (
+		<View style={styles.container}>
+			<StatusBar style='auto' />
+			<SafeAreaView>
+				<Home />
+				<AntDesign name={'stepforward'} size='24' />
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+				<Button
+					onPress={() => {
+						alert('You tapped the button!');
+					}}
+					title='Press Me'
+				/>
+			</SafeAreaView>
+		</View>
+	);
+}
